cddad0272c50751f2baaa11019395dbf18f42ce9
mujebpc-hub <mujebpc@gmail.com>
2026-06-22 17:26:30 +0530

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,8 @@
 const menuBtn = document.getElementById("menuBtn");
 const navLinks = document.getElementById("navLinks");
 
-menuBtn.onclick = () => {
-    navLinks.classList.toggle("active");
-};
+if(menuBtn){
+    menuBtn.addEventListener("click", () => {
+        navLinks.classList.toggle("active");
+    });
+}
