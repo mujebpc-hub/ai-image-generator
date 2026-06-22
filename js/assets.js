3e22120b78b885453f4498c9a944c5efbf70a5fb
mujebpc-hub <mujebpc@gmail.com>
2026-06-22 21:15:30 +0530

    Update assets.js

--- js/assets.js
+++ js/assets.js
@@ -33,11 +33,42 @@ fetch("./data/assets.json")
     // -----------------------------
     // ✅ ASSETS PAGE LOGIC
     // -----------------------------
-    data.forEach(renderCard);
+  const container = document.getElementById("assetsContainer");
+
+fetch("../data/assets.json")
+.then(response => response.json())
+.then(data => {
+
+    data.forEach(asset => {
+
+        const card = document.createElement("div");
+        card.classList.add("asset-card");
+
+        card.setAttribute("data-name", asset.title.toLowerCase());
+
+        card.innerHTML = `
+            <img src="${asset.cover}" alt="${asset.title}">
+            <h3>${asset.title}</h3>
+
+            <div style="display:flex; gap:8px; flex-direction:column;">
+
+                <a href="${asset.cover}" download>
+                    <button>Download</button>
+                </a>
+
+                <a href="preview.html?id=${asset.id}">
+                    <button>View</button>
+                </a>
+
+            </div>
+        `;
+
+        container.appendChild(card);
+
+    });
 
 })
 .catch(error => console.log(error));
-
 
 // -----------------------------
 // 🔥 CARD RENDER FUNCTION
