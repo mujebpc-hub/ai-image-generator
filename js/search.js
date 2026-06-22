9a2eced650b17f383c6e17078881c134188b26a1
mujebpc-hub <mujebpc@gmail.com>
2026-06-22 12:54:22 +0530

    Update search.js

--- js/search.js
+++ js/search.js
@@ -1,16 +1,33 @@
 const searchInput = document.getElementById("searchInput");
+const filterButtons = document.querySelectorAll(".filter-btn");
+const cards = document.querySelectorAll(".asset-card");
 
-searchInput.addEventListener("keyup", function () {
-    const value = this.value.toLowerCase();
-    const cards = document.querySelectorAll(".asset-card");
+if(searchInput){
+    searchInput.addEventListener("keyup", function () {
+        const value = this.value.toLowerCase();
 
-    cards.forEach(card => {
-        const title = card.dataset.name.toLowerCase();
+        document.querySelectorAll(".asset-card").forEach(card => {
+            const name = card.dataset.name;
 
-        if(title.includes(value)){
-            card.style.display = "block";
-        } else {
-            card.style.display = "none";
-        }
+            if(name.includes(value)){
+                card.style.display = "block";
+            } else {
+                card.style.display = "none";
+            }
+        });
+    });
+}
+
+filterButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        const filter = button.dataset.filter;
+
+        document.querySelectorAll(".asset-card").forEach(card => {
+            if(filter === "all" || card.dataset.category === filter){
+                card.style.display = "block";
+            } else {
+                card.style.display = "none";
+            }
+        });
     });
 });
