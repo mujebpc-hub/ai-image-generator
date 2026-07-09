b9865fb63a88cb7554d1e570610de6d178869839
mujebpc-hub <mujebpc@gmail.com>
2026-07-09 11:49:17 +0530

    Add image generation with pollinations provider

--- providers/image/manager.js
+++ providers/image/manager.js
@@ -1,1 +1,16 @@
+import { generateWithPollinations } from "./pollinations.js";
+import { APP_CONFIG } from "../../js/config.js";
 
+export async function generateImage(prompt, options = {}) {
+
+    switch (APP_CONFIG.imageProvider) {
+
+        case "pollinations":
+            return await generateWithPollinations(prompt, options);
+
+        default:
+            throw new Error("Image provider not supported.");
+
+    }
+
+}
